b002135cf3d63e86ebaf4feb4a7aaa54ad16ba8e
miguelmoranv <miguelonchas001@gmail.com>
2024-06-01 16:09:39 -0600

    v2

--- aplicacion/bd/db.php
+++ aplicacion/bd/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect("localhost","root","","sgdd");
+$conn=mysqli_connect("bfjhgnswxqlerifxenwj-mysql.services.clever-cloud.com","ur0xe82t8vl5jtsf","GY0qndEYXd8TjZUhosUo","bfjhgnswxqlerifxenwj");
 
 
 ?>
